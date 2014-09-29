05a337d75d6680ce44f146337997dafc215106e8
Jonathan Hood <me@jonathanhood.org>
2014-09-29 17:40:02 -0500

    More types of load instructions

--- gbc.py
+++ gbc.py
@@ -36,10 +36,11 @@ def copy_register(op, src, dest):
     def handler(processor, params):
         processor.registers[dest] = processor.registers[src]
 
-def load_from_memory(op,dest):
+def load_from_memory(op,dest,addr):
     @opcode(op=op,size=1,clocks=8)
     def handler(processor, params):
-        address = ( processor.registers["H"] << 8 ) + processor.registers["L"]
+        high_reg, low_reg = (addr[0], addr[1])
+        address = ( processor.registers[high_reg] << 8 ) + processor.registers[low_reg]
         processor.registers[dest] = processor.memory[address]
 
 def copy_to_memory(op,src):
@@ -48,14 +49,31 @@ def copy_to_memory(op,src):
         address = ( processor.registers["H"] << 8 ) + processor.registers["L"]
         processor.memory[address] = processor.registers[src]
 
+def copy_immediate_to_memory(op):
+    @opcode(op=op,size=2,clocks=12)
+    def handler(processor, params):
+        address = ( processor.registers["H"] << 8 ) + processor.registers["L"]
+        processor.memory[address] = params[0]
+
+def copy_from_immediate_address(op,dest):
+    @opcode(op=op,size=3,clocks=16)
+    def handler(processor, params):
+        address = ( params[1] << 8 ) + params[0]
+        processor.registers[dest] = processor.memory[address]
+    
+
 # Opcode Table
 
 load_8bit_immediate(op=0x06,dest="B")
+load_from_memory(op=0x0A,dest="A",addr="BC")
 load_8bit_immediate(op=0x0E,dest="C")
+load_from_memory(op=0x1A,dest="A",addr="DE")
 load_8bit_immediate(op=0x16,dest="D")
 load_8bit_immediate(op=0x1E,dest="E")
 load_8bit_immediate(op=0x26,dest="H")
 load_8bit_immediate(op=0x2E,dest="L")
+
+copy_immediate_to_memory(op=0x36)
 
 copy_register(op=0x40,src="B",dest="B")
 copy_register(op=0x41,src="C",dest="B")
@@ -63,7 +81,7 @@ copy_register(op=0x42,src="D",dest="B")
 copy_register(op=0x43,src="E",dest="B")
 copy_register(op=0x44,src="H",dest="B")
 copy_register(op=0x45,src="L",dest="B")
-load_from_memory(op=0x46,dest="B")
+load_from_memory(op=0x46,dest="B",addr="HL")
 
 copy_register(op=0x48,src="B",dest="C")
 copy_register(op=0x49,src="C",dest="C")
@@ -71,7 +89,7 @@ copy_register(op=0x4A,src="D",dest="C")
 copy_register(op=0x4B,src="E",dest="C")
 copy_register(op=0x4C,src="H",dest="C")
 copy_register(op=0x4D,src="L",dest="C")
-load_from_memory(op=0x4E,dest="C")
+load_from_memory(op=0x4E,dest="C",addr="HL")
 
 copy_register(op=0x50,src="B",dest="D")
 copy_register(op=0x51,src="C",dest="D")
@@ -79,7 +97,7 @@ copy_register(op=0x52,src="D",dest="D")
 copy_register(op=0x53,src="E",dest="D")
 copy_register(op=0x54,src="H",dest="D")
 copy_register(op=0x55,src="L",dest="D")
-load_from_memory(op=0x56,dest="D")
+load_from_memory(op=0x56,dest="D",addr="HL")
 
 copy_register(op=0x58,src="B",dest="E")
 copy_register(op=0x59,src="C",dest="E")
@@ -87,7 +105,7 @@ copy_register(op=0x5A,src="D",dest="E")
 copy_register(op=0x5B,src="E",dest="E")
 copy_register(op=0x5C,src="H",dest="E")
 copy_register(op=0x5D,src="L",dest="E")
-load_from_memory(op=0x5E,dest="E")
+load_from_memory(op=0x5E,dest="E",addr="HL")
 
 copy_register(op=0x60,src="B",dest="H")
 copy_register(op=0x61,src="C",dest="H")
@@ -95,7 +113,7 @@ copy_register(op=0x62,src="D",dest="H")
 copy_register(op=0x63,src="E",dest="H")
 copy_register(op=0x64,src="H",dest="H")
 copy_register(op=0x65,src="L",dest="H")
-load_from_memory(op=0x66,dest="H")
+load_from_memory(op=0x66,dest="H",addr="HL")
 
 copy_register(op=0x68,src="B",dest="L")
 copy_register(op=0x69,src="C",dest="L")
@@ -103,7 +121,7 @@ copy_register(op=0x6A,src="D",dest="L")
 copy_register(op=0x6B,src="E",dest="L")
 copy_register(op=0x6C,src="H",dest="L")
 copy_register(op=0x6D,src="L",dest="L")
-load_from_memory(op=0x6E,dest="L")
+load_from_memory(op=0x6E,dest="L",addr="HL")
 
 copy_to_memory(op=0x70,src="B")
 copy_to_memory(op=0x71,src="C")
@@ -118,8 +136,10 @@ copy_register(op=0x7A,src="D",dest="A")
 copy_register(op=0x7B,src="E",dest="A")
 copy_register(op=0x7C,src="H",dest="A")
 copy_register(op=0x7D,src="L",dest="A")
-load_from_memory(op=0x7E,dest="A")
+load_from_memory(op=0x7E,dest="A",addr="HL")
 copy_register(op=0x7F,src="A",dest="A")
+
+copy_from_immediate_address(op=0xFA,dest="A")
 
 
 def run_instruction(program, processor):
--- test_instructions.py
+++ test_instructions.py
@@ -44,20 +44,22 @@ def test_register_copies(opcode,src,dest):
     assert processor.registers[dest] == 100
 
 
-@pytest.mark.parametrize("opcode,dest", [
-    ("\x46", "B"),
-    ("\x4E", "C"),
-    ("\x56", "D"),
-    ("\x5E", "E"),
-    ("\x66", "H"),
-    ("\x6E", "L"),
-    ("\x7E", "A")
+@pytest.mark.parametrize("opcode,dest,high,low", [
+    ("\x0A", "A", "B", "C"),
+    ("\x1A", "A", "D", "E"),
+    ("\x46", "B", "H", "L"),
+    ("\x4E", "C", "H", "L"),
+    ("\x56", "D", "H", "L"),
+    ("\x5E", "E", "H", "L"),
+    ("\x66", "H", "H", "L"),
+    ("\x6E", "L", "H", "L"),
+    ("\x7E", "A", "H", "L")
 ])
-def test_load_from_memory(opcode,dest):
+def test_load_from_memory(opcode,dest,high,low):
     processor = gbc.Processor()
     processor.memory[0x0110] = 0x5A
-    processor.registers["H"] = 0x01
-    processor.registers["L"] = 0x10
+    processor.registers[high] = 0x01
+    processor.registers[low] = 0x10
     gbc.run_instruction(opcode, processor)
     assert processor.program_counter == 1
     assert processor.registers[dest] == 0x5A
@@ -84,3 +86,20 @@ def test_load_to_memory(opcode,src):
         assert processor.memory[0x0110] == 0x10
     else:
         assert processor.memory[0x0110] == 0x5A
+
+def test_copy_immediate_to_memory():
+    program = "\x36\x5A"
+    processor = gbc.Processor()
+    processor.registers["H"] = 0x01
+    processor.registers["L"] = 0x10
+    gbc.run_instruction(program, processor)
+    assert processor.program_counter == 2
+    assert processor.memory[0x0110] == 0x5A
+
+def test_copy_from_immediate_address():
+    program = "\xFA\x10\x01"
+    processor = gbc.Processor()
+    processor.memory[0x0110] = 0x5A
+    gbc.run_instruction(program, processor)
+    assert processor.program_counter == 3
+    assert processor.registers["A"] == 0x5A
