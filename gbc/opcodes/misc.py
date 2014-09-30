206f27e170f72346068a75ecca3464c665e9b849
Jonathan Hood <me@jonathanhood.org>
2014-09-29 21:00:07 -0500

    Cleanup of the misc file

--- gbc/opcodes/misc.py
+++ gbc/opcodes/misc.py
@@ -1,33 +1,23 @@
 from ..core import *
+ 
+@opcode(op=0x36,size=2,clocks=12)
+def copy_immediate_to_memory(processor, params):
+    address = ( processor.registers["H"] << 8 ) + processor.registers["L"]
+    processor.memory[address] = params[0]
 
-def load_from_memory_half_addr(op,dest,addr): 
-    @opcode(op=op,size=1,clocks=8)
-    def handler(processor, params):
-        low_reg = processor.registers[addr]
-        address = 0xFF00 + low_reg
-        processor.registers[dest] = processor.memory[address]
-
-def copy_to_memory_half_addr(op,src,addr):
-    @opcode(op=op,size=1,clocks=8)
-    def handler(processor, params):
-        address = 0xFF00 + processor.registers[addr]
-        processor.memory[address] = processor.registers[src]
-
-def copy_immediate_to_memory(op):
-    @opcode(op=op,size=2,clocks=12)
-    def handler(processor, params):
-        address = ( processor.registers["H"] << 8 ) + processor.registers["L"]
-        processor.memory[address] = params[0]
-
-def copy_from_immediate_address(op,dest):
-    @opcode(op=op,size=3,clocks=16)
-    def handler(processor, params):
-        address = ( params[1] << 8 ) + params[0]
-        processor.registers[dest] = processor.memory[address]
+@opcode(op=0xE2,size=1,clocks=8)
+def copy_to_memory_half_addr(processor, params):
+    address = 0xFF00 + processor.registers["C"]
+    processor.memory[address] = processor.registers["A"]
     
+@opcode(op=0xF2,size=1,clocks=8)
+def load_from_memory_half_addr(processor, params):
+    low_reg = processor.registers["C"]
+    address = 0xFF00 + low_reg
+    processor.registers["A"] = processor.memory[address]
 
-copy_immediate_to_memory(op=0x36)
-copy_to_memory_half_addr(op=0xE2,src="A",addr="C")
-load_from_memory_half_addr(op=0xF2,dest="A",addr="C")
-copy_from_immediate_address(op=0xFA,dest="A")
+@opcode(op=0xFA,size=3,clocks=16)
+def handler(processor, params):
+    address = ( params[1] << 8 ) + params[0]
+    processor.registers["A"] = processor.memory[address]
  
