31b88854be0a29ebb88a7f1e2ca828525bacb5a3
Jonathan Hood <me@jonathanhood.org>
2015-02-10 19:59:45 -0600

    Cover some negative cases

--- test/test_core.py
+++ test/test_core.py
@@ -1,5 +1,5 @@
 import pytest
-from gbc.core import Registers, print_opcodes
+from gbc.core import Registers, Flags, print_opcodes
 
 def test_printing_all_opcodes():
     print_opcodes()
@@ -13,3 +13,16 @@ def test_throw_on_bad_read():
     regs = Registers()
     with pytest.raises(IndexError):
         foo = regs["xy"]
+
+def test_throw_on_bad_flag_read():
+    flags = Flags()
+    with pytest.raises(IndexError):
+        foo = flags["something"]
+
+def test_throw_on_bad_flag_write():
+    flags = Flags()
+    with pytest.raises(IndexError):
+        flags["something"] = False
+    with pytest.raises(ValueError):
+        flags["zero"] = "hello"
+
